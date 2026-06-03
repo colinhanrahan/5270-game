234ba9e9d10aacaa497818a7b20e842e4c2265d0
colinhanrahan <colinhanrahan9@gmail.com>
2026-06-03 15:25:39 -0700

    Fix server path?

--- quicktionary/export/index.service.worker.js
+++ quicktionary/export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780523981|458607815';
+const CACHE_VERSION = '1780525415|1789474411';
 /** @type {string} */
 const CACHE_PREFIX = 'Quicktionary-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
